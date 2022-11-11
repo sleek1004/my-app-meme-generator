d1221da194c29cc701e48d84d029bc52835e5524
sleek1004 <arojojoyeo@yahoo.com>
2022-11-11 23:48:13 +0100

    using state to pass data

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
     return(
         <header className='header'>
             <img  src={troll} className="header-img"
-             />
+             alt=""/>
             <h2 className='header-title'>Header component</h2>
             <h4 className='header-project'>React Course - Project 3</h4>
         </header>
--- src/components/Meme.jsx
+++ src/components/Meme.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import memesData  from './memesData'
 
 
-
-
 export default function Meme() {
+
+//const [memeImage, setMemeImage]= React.useState("memeImage")
+const [meme,setMeme] = React.useState({
+        topText : "",
+        bottomText : "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+})
+    const[allMemeImages, setAllMemeImages] = React.useState(memesData)
 
 
     function getMemeImage(){
-       const memesArray = memesData.data.memes
-     
+       const memesArray = allMemeImages.data.memes
+       const randomNumber = Math.floor(Math.random()* memesArray.length)
+        const url = memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage:url
+        }))
+        
         
     }
     return(
@@ -22,8 +34,9 @@ export default function Meme() {
            <input type='text'
            placeholder='Button text'
            className='data'></input>
-           <button className='meme-button' onClick={getMemeImage}>Get a new meme image  🖼</button>
+           <button className='meme-button' onClick={getMemeImage} >Get a new meme image  🖼</button>
            </form>
+           <img src={meme.randomImage} alt="" />
         </div>
     )
 }
